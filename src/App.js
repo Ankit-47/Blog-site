135325acf82ca5ac7bca5c21399274873d9e2fbd
Ankit-47 <omkathet219@gmail.com>
2024-04-23 22:20:11 +0545

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Home from "./pages/home/Home";
+import TopBar from "./topbar/TopBar";
+import Header from './header/Header';
+
+
+function App() {
+  return (
+    <>
+      <TopBar />
+      <Header />
+      <Home />
+    </>
+  );
+}
+
+export default App;
