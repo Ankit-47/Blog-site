f26e9008a54d5c0cbc48f4230df12a3c85002f56
Ankit-47 <omkathet219@gmail.com>
2024-04-24 23:25:09 +0545

    sidebar and more pages are added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+
+import Header from "../../header/Header"
+import Posts from "../../posts/Posts";
+import Sidebar from "../../sidebar/Sidebar"
+import "./home.css";
+
 
 export default function Home() {
   return (
+  <>
+    <Header /> 
     <div className='home'>
-      homepage
+      <Posts />
+      <Sidebar />
     </div>
+  </>
   );
 }
---
+++ src/sidebar/Sidebar.jsx
@@ -0,0 +1,36 @@
+import "./sidebar.css"
+
+export default function Sidebar() {
+  return (
+    <div className='sidebar'>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">ABOUT ME</span>
+        <img
+          src="https://themegoods-cdn-pzbycso8wng.stackpathdns.com/grandblog/demo/wp-content/uploads/2015/11/aboutme.jpeg"
+          alt=""
+        />
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+            Eaque fugit minima veritatis corrupti placeat officia ratione, ad culpa amet aliquid repellendus, pariatur, laboriosam officiis totam nisi saepe eos error necessitatibus.</p>
+      </div>
+      <div className="sidebarItems">
+        <span className="sidebarTitle">CATEGORIES</span>
+        <ul className="sidebarList">
+            <li className="sidebarListItem">Life</li>
+            <li className="sidebarListItem">Music</li>
+            <li className="sidebarListItem">Style</li>
+            <li className="sidebarListItem">Sport</li>
+            <li className="sidebarListItem">Tech</li>
+            <li className="sidebarListItem">Cinema</li>
+        </ul>
+      </div>
+      <div className="sidebarItem">
+      <span className="sidebarTitle">FOLLOW US</span>
+      <div className="sidebarSocial">
+        <i className="sidebarIcon fa-brands fa-square-facebook"></i>
+        <i className="sidebarIcon fa-brands fa-square-instagram"></i>
+        <i className="sidebarIcon fa-brands fa-github"></i>
+      </div>
+      </div>
+    </div>
+  )
+}
